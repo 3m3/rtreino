4de38571527fc68b104b9eb278f4cc2e1bad645c
Rafael Crivellari Saliba Schouery <schouery@gmail.com>
2009-06-24 09:47:57 -0300

    PKU get status almost finished

--- lib/pku.rb
+++ lib/pku.rb
@@ -8,8 +8,30 @@ class PKU
   def self.submissions_for(user)
     Timeout::timeout(1) do
       response = open("http://acm.pku.edu.cn/JudgeOnline/status?user_id=rtreino")
-      Hpricot(response)
-      "5137643	rtreino	1000	Wrong Answer			GCC	30B	2009-05-12 06:49:32"
+      doc = Hpricot(response)
+      h = nil
+      hashes = []
+      doc.search("//table[@class='a']").first.search("tr").each do |l|
+        if h.nil? #primeira linha
+          h = []
+          l.search("td").each do |t|
+            h << t.html
+          end
+        else
+          s = Hash.new
+          l.search("td").each_with_index do |t,i|        
+            t = t.search("a").first if !t.search("a").empty?
+            t = t.search("font").first if !t.search("font").empty?
+            s[h[i]] = t.html
+          end
+          hashes << s
+        end
+      end
+      s = ""
+      h.each do |l|
+        s += hashes[0][l] + " "
+      end
+      s.chomp
     end
   end
   
--- spec/lib/pku_spec.rb
+++ spec/lib/pku_spec.rb
@@ -10,7 +10,6 @@ describe PKU do
     PKU.should respond_to :submissions_for
     html = open(File.dirname(__FILE__) + "/PKU_examples/one_submission.html")
     PKU.should_receive(:open).with("http://acm.pku.edu.cn/JudgeOnline/status?user_id=rtreino").and_return(html)
-    PKU.should_receive(:Hpricot).with(html)
     PKU.submissions_for("rtreino").should =~ /5137643\s+rtreino\s+1000\s+Wrong Answer\s+GCC\s+30B\s+2009-05-12\s+06:49:32/
   end
   
