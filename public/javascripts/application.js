b228b4cbb0c0c1e758252281c2db1fad1bed0f47
Wanderley Guimarães <wanderley.guimaraes@gmail.com>
2009-10-01 15:55:41 -0300

    Focus to the first input in form pages

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,3 +11,13 @@ function showTab(divParent, tabSelected) {
       }
     });
 }
+
+// Focus the first control in page
+Event.observe(window, 'load', function() {
+  var e = $A(document.getElementsByTagName('*')).find(function(e) {
+    return (e.tagName.toUpperCase() == 'INPUT' && (e.type == 'text' || e.type == 'password'))
+        || e.tagName.toUpperCase() == 'TEXTAREA' || e.tagName.toUpperCase() == 'SELECT';
+  });
+  if (e) e.focus();
+});
+
